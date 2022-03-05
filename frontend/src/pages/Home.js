557a7b93a00859214707c99cb0f3602bab32f9d5
Bimansyah Pratama <bimansyahpratama@gmail.com>
2022-03-06 02:02:49 +0700

    Homework Done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
